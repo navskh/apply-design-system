c6c69033e50aad72da3998f6fd79729aeeeb50d5
Young <navskh@naver.com>
2023-12-11 22:51:47 +0900

    일단 여기까지

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css'; // CSS Module 사용 예시
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>ADS (ApplyDesignSystem)</div>
+      <div className={styles.logo}> <a href="/">ADS (ApplyDesignSystem)</a></div>
       <input type="search" placeholder="Search..." className={styles.searchInput} />
     </header>
   );
