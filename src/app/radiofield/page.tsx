68c9255a24909c85036208ab095a5fa6b9ee91f8
Young <navskh@naver.com>
2023-12-11 22:47:13 +0900

    radio 수정

--- src/app/radiofield/page.tsx
+++ src/app/radiofield/page.tsx
@@ -12,23 +12,23 @@ export default function RadioField() {
           <span class="cell_box radio_col2" style={{ cursor: "pointer" }} />
           <span class="cell_radio1 dim">
               <input type="radio" id="r1" name="rr" />
-              <label htmlFor="r1">일반편입학(3학년)</label>
+              <label for="r1">일반편입학(3학년)</label>
           </span>
           <span class="cell_radio1">
               <input type="radio" id="r2" name="rr" />
-              <label htmlFor="r2">일반편입학(3학년)</label>
+              <label for="r2">일반편입학(3학년)</label>
           </span>
           <span class="cell_radio1">
               <input type="radio" id="r3" name="rr" disabled />
-              <label htmlFor="r3">일반편입학(3학년)</label>
+              <label for="r3">일반편입학(3학년)</label>
           </span>
           <span class="cell_radio1">
               <input type="radio" id="r4" name="rr" />
-              <label htmlFor="r4">일반편입학(3학년)</label>
+              <label for="r4">일반편입학(3학년)</label>
           </span>
           <span class="cell_radio1">
               <input type="radio" id="r5" name="rr" />
-              <label htmlFor="r5">사회통합전형</label>
+              <label for="r5">사회통합전형</label>
           </span>
       </div>
   </div>`
