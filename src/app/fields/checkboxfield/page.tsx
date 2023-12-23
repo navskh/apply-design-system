5bd4b6c237deaad32a3875566505a0e34efdf3d8
Young <navskh@naver.com>
2023-12-23 16:33:25 +0900

    feat : page 들 추가

--- src/app/fields/checkboxfield/page.tsx
+++ src/app/fields/checkboxfield/page.tsx
@@ -1,31 +1,8 @@
 'use client';
-import React, { useEffect, useState } from 'react';
-import hljs from 'highlight.js';
-import 'highlight.js/styles/atom-one-dark.css'; // 스타일 선택
-import beautify from 'js-beautify';
-import styles from '@/app/layout.module.css';
-import ExampleTab from '@/app/components/ExampleTab';
+import Renderer from '@/app/components/renderer';
 
 export default function CheckboxField() {
-  const [sampleHTML, setSampleHTML] = useState('');
-
-  useEffect(() => {
-    fetch('/html/checkboxfield.html')
-      .then(response => response.text())
-      .then(data => setSampleHTML(data));
-  }, []);
-
-
-  const formattedHtmlCode = beautify.html(sampleHTML);
-
-  useEffect(() => {
-    hljs.highlightAll();
-  }, []);
-
   return (
-    <>
-      <h1 className={styles.title}>체크박스 (CheckboxField)</h1>
-      <ExampleTab sampleHTML={formattedHtmlCode} />
-    </>
+    <Renderer title="체크박스 (CheckboxField)" />
   );
 }
