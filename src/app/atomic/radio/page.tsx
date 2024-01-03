2e30524c744c060977dac6d6f79787accd7f44da
Young <navskh@naver.com>
2024-01-03 16:48:49 +0900

    path 추가

--- src/app/atomic/radio/page.tsx
+++ src/app/atomic/radio/page.tsx
@@ -1,8 +1,11 @@
 'use client';
 import Renderer from '@/app/components/renderer';
+import { usePathname } from 'next/navigation';
 
 export default function Page() {
+  const path = usePathname();
+
   return (
-    <Renderer title="라디오" />
+    <Renderer title="라디오" path={path} />
   );
 }
